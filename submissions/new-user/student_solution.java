a8c4b82f1355a2a47ad6eeaa0a09f001d89715ad
subitha-Mohanasundaram <subi.re17@gmail.com>
2026-03-05 11:52:37 +0530

    Test unmapped-user fallback and issue creation

---
+++ submissions/new-user/student_solution.java
@@ -0,0 +1,11 @@
+public class student_solution {
+    public static double addNumbers(double a, double b) {
+        return a + b;
+    }
+}
+// force-java-language
+// verify-java-language
+// auto-java-check
+// verify-cheat-email-optimization
+
+
